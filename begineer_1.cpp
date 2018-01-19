ba7362539c942fb635436a66426e1c10ba10ae93
Romitrcv <32649280+Romitrcv@users.noreply.github.com>
2018-01-19 18:02:05 +0530

    Add files via upload

---
+++ begineer_1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("INPUT\n");
+	scanf("%d",&n);
+	printf("OUTPUT\n");
+	if(n>0)
+	printf("positive");
+	else if(n<0)
+	printf("negative");
+	else
+	printf("zero");
+}
